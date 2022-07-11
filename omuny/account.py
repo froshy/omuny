41a49c680d201ceab249e3d1beb96ca06cfedff0
froshy <rgl78@cornell.edu>
2022-07-10 22:12:51 -0400

    Implemented basic account class attribute methods Added implementations of oweing a single person and total oweing. Added some class attributes

--- omuny/account.py
+++ omuny/account.py
@@ -3,12 +3,48 @@ class account():
      A class to represent an account
     """
 
-    def __init__():
+    def __init__(uname, pword):
+        self.total_owed_to_others = 0
+        self.total_owed_to_you = 0
+        self.pword = pword
+        self.uname = uname
+        self.reports = {}
+        self.owed_to_one = {}
+
+    def get_owed_to_you(self, account) -> String:
+        return account.owed_to_one[self]
+    
+    def get_owed(self, account) -> String:
+        return self.owed_to_one[account]
+
+    def get_total_owed_to_you(self) -> String:
+        return self.total_owed_to_you
+
+    def get_total_owed_to_others(self) -> String:
+        return self.total_owed_to_others
+    
+    def get_uname(self) -> String:
+        return self.uname
+
+    def set_pword(self,pword) -> String:
+        if verify_pword():
+            self.pword = pword
+
+    def add_owe_to_you(self) -> String:
         pass
 
-    def get_owe():
+    def add_owe_other(self) -> String:
         pass
     
-    def get_owed():
+    def verify_pword(vpword) -> bool:
         pass
+    
+
+
+    
+    def __repr__() -> String:
+        pass
+    
+
+    
 
